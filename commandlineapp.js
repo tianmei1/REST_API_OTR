02520999437f077a9474ecc68374c99c1c3ec3e1
tianmei1 <tmei1@hawk.iit.edu>
2018-08-05 15:01:39 -0500

    Filter records and store into array. change records data in files.

--- commandlineapp.js
+++ commandlineapp.js
@@ -46,26 +46,77 @@ console.log("*Output-1: sorted by gender (females before males) then by last nam
 console.log("*Output-2: sorted by birth date, ascending.");
 console.log("*Output-3: sorted by last name, descending.");
 console.log('---------------------------------------------------------------------------------------');
+get_files(function (result) { 
 prompts.question("Please enter the output type number. Type 1 or 2 or 3:" + "   ", function (inputNumber) {
     var messgaeForInputFileName = "";
+    var recordsArray=[];
     if (inputNumber === '1') {
         messgaeForInputFileName = "The output-" + inputNumber + " is as following:";
-        // get_files(function (result) { console.log(result); 
-        // });
+        console.log(messgaeForInputFileName);
+        var txtContent = [];
+        for (var key in result) {
+            if (result.hasOwnProperty(key)) {
+                txtContent.push(result[key]);
+            }
+        }
+        //put all records from all files into recordsArray.
+        for(var j=0; j<txtContent.length;j++){
+            var singleFilerecordArray = readRecordByLine(txtContent[j]);
+            for(var i = 0 ; i <singleFilerecordArray.length ; i++){
+                recordsArray.push(singleFilerecordArray[i]);
+            }
+        }
+        sortToOutput1(recordsArray);
+        console.log(recordsArray);
     }
     else if (inputNumber === '2') {
         messgaeForInputFileName = "The output-" + inputNumber + " is as following:";
+        console.log(messgaeForInputFileName);
     }
     else if (inputNumber === '3') {
         messgaeForInputFileName = "The output-" + inputNumber + " is as following:";
+        console.log(messgaeForInputFileName);
     }
     else {
         messgaeForInputFileName = "Sorry, You can only enter 1 or 2 or 3";
+        console.log(messgaeForInputFileName);
     }
-    console.log(messgaeForInputFileName);
     process.exit();
 });
+});
 
+function sortToOutput1(records){
+    for(var i=0; i< records.length; i++){
+        
+    }
+}
+function sortToOutput2(txtstring){
+
+}
+function sortToOutput2(txtstring){
+
+}
+//get every single record from signle file into one string array. 
+function readRecordByLine(txtstring){
+    var singleFileArray = txtstring.split('\r\n');
+    var singleRecordArray =[];
+    if(singleFileArray[0].includes(',')){     
+        for(var i=0; i< singleFileArray.length; i++){
+            singleRecordArray.push(singleFileArray[i].split(','));
+        }
+    }
+    else if(singleFileArray[0].includes('|')){
+        for(var i=0; i< singleFileArray.length; i++){
+            singleRecordArray.push(singleFileArray[i].split('|'));
+        }
+    }
+    else if(singleFileArray[0].includes(' ')){
+        for(var i=0; i< singleFileArray.length; i++){
+            singleRecordArray.push(singleFileArray[i].split(' '));
+        }
+    }
+    return singleRecordArray;
+}
 // console.log(fileNameArray);
 // console.log(fileNumber);
 // console.log(fileContents["Comma-delimited.txt"]);
